351986b82a00ffb93aeae52c394931555834ee98
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-02-25 21:28:55 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,18 @@
+﻿using Business.DTOs.Requests;
+using Business.DTOs.Responses;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts;
+
+public interface IBrandService
+{
+    CreatedBrandResponse Add(CreateBrandRequest brand);
+    List<Brand> GetAll();
+}
+//responses and requests
+//yanıtlar için ayrı dto, request için ayrı dto kullanılmalı ikisi içinde aynı dto kullanımı sektörde yaygın bir hatadır.!
---
+++ DataAccess/Concretes/BrandDal.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes;
+
+public class BrandDal : IBrandDal
+{
+    private List<Brand> _brandList;
+
+    public BrandDal()
+    {
+        _brandList = new List<Brand>();
+        _brandList.Add(new Brand("Tofaş"));
+        _brandList.Add(new Brand("Renault"));
+        _brandList.Add(new Brand("Opel"));
+    }
+
+    public void Add(Brand brand)
+    {
+        _brandList.Add(brand);
+    }
+
+    public List<Brand> GetAll()
+    {
+        return _brandList;
+    }
+}
